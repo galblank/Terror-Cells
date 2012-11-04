130ca64a147550ef9f2d5f51117ac3c687c69b96
Gal Blank <galblank@Gal-Blanks-MacBook-Pro.local>
2012-11-04 07:07:04 -0500

    Initial Commit

---
+++ Terror Cells/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Terror Cells
+//
+//  Created by Gal Blank on 11/4/12.
+//  Copyright (c) 2012 Gal Blank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ Terror Cells/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Terror Cells
+//
+//  Created by Gal Blank on 11/4/12.
+//  Copyright (c) 2012 Gal Blank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
